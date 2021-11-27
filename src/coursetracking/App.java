d3f82afbe40c36d9f2a508d8aebe00e2c8961d0c
ilayydaoruc <74316992+ilayydaoruc@users.noreply.github.com>
2021-11-27 18:46:48 +0300

    input reading implemented
    Reading input file into the config object.
    input contains the registration term and courses.

--- src/coursetracking/App.java
+++ src/coursetracking/App.java
@@ -1,14 +1,46 @@
 package coursetracking;
 
-import com.google.gson.Gson;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Scanner;
 
-import coursetracking.models.Transcript;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+import coursetracking.models.Course;
+import coursetracking.utils.Utils;
 
 /**
  * App
  */
 public class App {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        App app = new App();
+        app.start();
     }
+
+    public void start() throws Exception {
+
+        File myObj = Utils.getInstance().getResource("input.json");
+        String data = "";
+        Scanner myReader = new Scanner(myObj);
+        while (myReader.hasNextLine()) {
+            data += myReader.nextLine();
+        }
+        myReader.close();
+        Config c = new Gson().fromJson(data, Config.class);
+        System.out.println(c);
+    }
+}
+
+class Config { 
+    public String registrationTerm;
+    public Course[] courses ;
+    @Override
+    public String toString() {
+        return "Config [courses=" + Arrays.toString(courses) + ", semester=" + registrationTerm + "]";
+    }
+    
 }
--- src/coursetracking/utils/Utils.java
+++ src/coursetracking/utils/Utils.java
@@ -1,5 +1,6 @@
 package coursetracking.utils;
 
+import java.io.File;
 import java.util.HashMap;
 
 public class Utils {
@@ -16,4 +17,11 @@ public class Utils {
     public float getGPAOfLetterGrade(String letterGrade) {
         return lettersHashMap.get(letterGrade);
     }
+
+    private File resources;
+    public File getResource(String filename){ 
+        if (resources == null) resources = new File("./resources");
+        return new File(resources,filename);
+    }
+
 }
