9fa91861f644ed7a69956eb2cbb2d02f54d92afb
Ritotwo <eeren881@gmail.com>
2021-12-23 21:43:22 +0300

    iteration2 created

---
+++ iteration2/code/src/coursetracking/models/Config.java
@@ -0,0 +1,15 @@
+package coursetracking.models;
+
+import java.util.ArrayList;
+
+public class Config {
+    public String registrationTerm;
+    public ArrayList<Course> curriculum;
+    public ArrayList<Course> electives;
+}
+
+class Elective {
+    public String type;
+    public int creditRequirement;
+    public ArrayList<Course> courses;
+}
---
+++ iteration2/code/src/coursetracking/models/Course.java
@@ -0,0 +1,62 @@
+package coursetracking.models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Course {
+    protected String courseCode;
+    protected String courseName;
+    protected int credit;
+    protected Course[] prerequisities;
+    protected int semester;
+    private String type;
+
+    public Course(Course c) {
+        this.courseCode = c.courseCode;
+        this.courseName = c.courseName;
+        this.credit = c.credit;
+        this.prerequisities = c.prerequisities;
+        this.semester = c.semester;
+        this.type = c.type;
+    }
+
+    public Course[] getPrerequisites() {
+        return prerequisities;
+    }
+
+    public String getCourseName() {
+        return courseCode;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((courseCode == null) ? 0 : courseCode.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Course other = (Course) obj;
+        if (courseCode == null) {
+            if (other.courseCode != null)
+                return false;
+        } else if (!courseCode.equals(other.courseCode))
+            return false;
+        return true;
+    }    
+}
